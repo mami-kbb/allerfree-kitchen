cd694993bb5d86343e279fbec10d2dd92c676ba3
mami <kbb33m0730@gmail.com>
2026-09-04 12:02:57 +0900

    ルートからDB専用ルートを削除

--- routes/web.php
+++ routes/web.php
@@ -39,20 +39,3 @@ Route::middleware(['auth', 'verified'])->group(function() {
     Route::put('/recipe/{recipe_id}/edit', [RecipeController::class, 'update'])->name('recipe.update');
     Route::delete('/recipe/{recipe_id}/delete', [RecipeController::class, 'delete'])->name('recipe.delete');
 });
-
-Route::get('/reset-data-xyz9f3', function () {
-    if (request('key') !== env('SEED_SECRET')) {
-        abort(403);
-    }
-
-    DB::table('likes')->delete();
-    DB::table('comments')->delete();
-    DB::table('ingredient_recipe')->delete();
-    DB::table('allergy_recipe')->delete();
-    DB::table('steps')->delete();
-    DB::table('recipes')->delete();
-
-    Artisan::call('db:seed', ['--force' => true]);
-
-    return 'Reset & Seeding done: ' . Artisan::output();
-});
